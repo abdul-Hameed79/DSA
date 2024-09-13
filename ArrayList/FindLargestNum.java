1258e4c089a41c53d2d0d08074797fecb64cbf7a
Abdul Hameed <sd.hameed79@gmail.com>
2024-09-13 21:59:00 +0530

    Finding largest number in the given arraylist

---
+++ ArrayList/FindLargestNum.java
@@ -0,0 +1,22 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class FindLargestNum {
+    public static void main(String[] args) {
+        ArrayList<Integer> values = new ArrayList<>();
+        values.add(2);
+        values.add(5);
+        values.add(9);
+        values.add(3);
+        values.add(6);
+
+        int maxNum = 0;
+        for(int i=0; i<values.size(); i++) {
+            if(maxNum < values.get(i)) {
+                maxNum = values.get(i);
+            }
+        }
+        System.out.println("Largest Number in the arraylist is: " + maxNum);
+    }
+}
+// O(n)
