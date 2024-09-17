2d9c4facf5b1ab444b60113a0065b064e3c96e88
Abdul Hameed <sd.hameed79@gmail.com>
2024-09-18 00:55:27 +0530

    Insertion sorting technique added

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,5 +1,29 @@
 package Sorting;
+import java.util.Arrays;
 
 public class InsertionSort {
-    
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(arr));
+    }
 }
