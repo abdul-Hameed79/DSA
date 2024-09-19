1230e1c7071c279cdf0e596dc17c6b55e98f5234
Abdul Hameed <sd.hameed79@gmail.com>
2024-09-19 22:45:39 +0530

    Insertion sort problem added

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -6,7 +6,7 @@ public class InsertionSort {
     public static void insertionSort(int[] arr) {
         int n = arr.length;
 
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
 
