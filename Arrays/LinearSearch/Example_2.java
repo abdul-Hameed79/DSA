daef4ca8b8a819b91631cb35a2fd16534cd5adc4
Abdul Hameed <sd.hameed79@gmail.com>
2024-12-26 14:41:17 +0530

    Comments added

--- Arrays/LinearSearch/Example_2.java
+++ Arrays/LinearSearch/Example_2.java
@@ -3,20 +3,19 @@
 package Arrays.LinearSearch;
 
 public class Example_2 {
-  static int findLargest(int[] arr) {
+  static void findLargest(int[] arr) {
     int LarNum = arr[0];        // initial Largest Num
     for(int i=1; i<arr.length; i++) {
         if(LarNum < arr[i]) {
             LarNum = arr[i];    // updted Largest Num
         }
     }
-    return LarNum;
+    System.out.println(LarNum);
   }
 
   // Main.java
   public static void main(String[] args) {
     int[] arr = {3, 2, 6, 9, 1};
-
-    System.out.println("Largest element in the array is: " + findLargest(arr));
+    findLargest(arr);
   }
 }
